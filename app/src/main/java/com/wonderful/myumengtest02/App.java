13a4d5bec9a7779ccb95da820917855de41b1e22
KXwonderful <abc1232008>
2016-08-31 19:25:21 +0800

    Initial commit

---
+++ app/src/main/java/com/wonderful/myumengtest02/App.java
@@ -0,0 +1,29 @@
+package com.wonderful.myumengtest02;
+
+import android.app.Application;
+
+import com.umeng.socialize.PlatformConfig;
+
+/**
+ * Created by ntop on 15/7/8.
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    //各个平台的配置，建议放在全局Application或者程序入口
+    {
+        //微信 wx12342956d1cab4f9,a5ae111de7d9ea137e88a5e02c07c94d
+        PlatformConfig.setWeixin("wx967daebe835fbeac", "5bb696d9ccd75a38c8a0bfe0675559b3");
+        //豆瓣RENREN平台目前只能在服务器端配置
+        //新浪微博
+        PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad");
+        //易信
+        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
+
+
+    }
+}
